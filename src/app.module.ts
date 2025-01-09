44f3bce530aa4b946f79b25cde53604bd8fa3bca
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2025-01-09 08:52:08 +0100

    prisma hozzá adva a modulokhoz

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { UserModule } from './user/user.module';
 import { PurchaseModule } from './purchase/purchase.module';
 import { ExpenseModule } from './expense/expense.module';
 import { BankAccountsModule } from './bank-accounts/bank-accounts.module';
-import { UserTypeModule } from './user-type/user-type.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [UserModule, PurchaseModule, ExpenseModule, BankAccountsModule, UserTypeModule],
+  imports: [UserModule, PurchaseModule, ExpenseModule, BankAccountsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
