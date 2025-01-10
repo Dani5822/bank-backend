33d436cd60403bf8fb527209b1dd4fa39c5ace2e
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2025-01-10 13:04:55 +0100

    purchase create kesz

--- src/purchase/dto/create-purchase.dto.ts
+++ src/purchase/dto/create-purchase.dto.ts
@@ -1,14 +1,9 @@
-import { BankAccounts, User } from "@prisma/client";
-
 export class CreatePurchaseDto {
-    id:number;
     total:number;
     Product: string;
     Vendor: string;
-    User:User[];
-    BankAccount: BankAccounts;
-    createdAt: Date;
-    updatedAt: Date;
+    userid: string[];
+    bankaccountid: string;
     replicationammount: number;
     replicationmetric: string;
     replicationstart: Date;
--- src/purchase/purchase.service.ts
+++ src/purchase/purchase.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePurchaseDto } from './dto/create-purchase.dto';
 import { UpdatePurchaseDto } from './dto/update-purchase.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class PurchaseService {
+  db: PrismaService;
+  constructor(db:PrismaService) {this.db=db};
   create(createPurchaseDto: CreatePurchaseDto) {
-    return 'This action adds a new purchase';
+    return this.db.purchase.create({
+      data: {
+        total: createPurchaseDto.total,
+        Product: createPurchaseDto.Product,
+        Vendor: createPurchaseDto.Vendor,
+        replicationammount: createPurchaseDto.replicationammount,
+        replicationmetric: createPurchaseDto.replicationmetric,
+        replicationstart: new Date(createPurchaseDto.replicationstart),
+        replicationend: new Date(createPurchaseDto.replicationend),
+        User: {
+          connect: { id: createPurchaseDto.userid[0] }
+        },
+        Bankaccount: {
+          connect: { id: createPurchaseDto.bankaccountid }
+        }
+      }
+    });
   }
 
   findAll() {
