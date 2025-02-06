a77c5f56defb7ba18e131afda8eb2150ebf3381f
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2025-02-06 14:30:45 +0100

    minden test sikeres

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { PrismaService } from './prisma.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
+      controllers: [AppController,],
       providers: [],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+  });
+
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
