da7bdbb647e83356fc3727e0cb659d43310e514e
Dani5822 <75309763+Dani5822@users.noreply.github.com>
2025-02-27 14:53:36 +0100

    swagger kiegeszitve

--- src/income/income.controller.ts
+++ src/income/income.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, NotFoundException, BadGatewayException, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, NotFoundException, BadRequestException } from '@nestjs/common';
 import { IncomeService } from './income.service';
 import { CreateIncomeDto } from './dto/create-income.dto';
 import { UpdateIncomeDto } from './dto/update-income.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiBody, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('incomes')
 @Controller('Income')
@@ -12,18 +12,23 @@ export class IncomeController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new income' })
+  @ApiBody({ type: CreateIncomeDto })
+  @ApiResponse({ status: 201, description: 'The income has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
   @UseGuards(AuthGuard)
   create(@Body() createIncomeDto: CreateIncomeDto) {
-    try{
+    try {
       return this.IncomeService.create(createIncomeDto);
-    }catch{
+    } catch {
       throw new BadRequestException('Bad Request');
     }
   }
 
-
   @Get(':id')
   @ApiOperation({ summary: 'Get an income by ID' })
+  @ApiParam({ name: 'id', required: true, description: 'Income ID' })
+  @ApiResponse({ status: 200, description: 'The income has been successfully retrieved.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
   @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     const income = this.IncomeService.findOne(+id);
@@ -36,6 +41,10 @@ export class IncomeController {
 
   @Patch(':id')
   @ApiOperation({ summary: 'Update an income by ID' })
+  @ApiParam({ name: 'id', required: true, description: 'Income ID' })
+  @ApiBody({ type: UpdateIncomeDto })
+  @ApiResponse({ status: 200, description: 'The income has been successfully updated.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
   @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateIncomeDto: UpdateIncomeDto) {
     return this.IncomeService.update(id, updateIncomeDto);
@@ -43,6 +52,9 @@ export class IncomeController {
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete an income by ID' })
+  @ApiParam({ name: 'id', required: true, description: 'Income ID' })
+  @ApiResponse({ status: 200, description: 'The income has been successfully deleted.' })
+  @ApiResponse({ status: 404, description: 'Not Found.' })
   @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.IncomeService.remove(id);
